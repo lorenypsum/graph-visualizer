519d46d1e28497a1a407959f74d2ce8e2ad6dc1c
samyhad <samira.had.2000@gmail.com>
2025-05-20 18:16:40 -0300

    Sidebar structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./pages/**/*.html",
     "./index.html",
     "./**/*.{js,ts,jsx,tsx,py}",
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
