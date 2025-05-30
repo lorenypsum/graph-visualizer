dca36205867931fbeb171d1e1fbd86bf0f47fa84
samyhad <samira.had.2000@gmail.com>
2025-05-30 20:08:42 -0300

    Finalmente terminei a nova página de draw graph

--- scripts/new_draw_page.py
+++ scripts/new_draw_page.py
@@ -7,16 +7,41 @@ import json
 # mpl.use('Agg')
 # import matplotlib.pyplot as plt
 
+
 def get_graph_from_js():
     data = json.loads(window.graph_json)
-    # nodes = [n['data']['id'] for n in data['nodes']]
-    # edges = [(e['data']['source'], e['data']['target'], e['data'].get('weight', 1)) for e in data['edges']]
     print("Grafo recebido do JS:", data)
     return data
 
+def cytoscape_to_networkx(data):
+    G = nx.DiGraph()
+    # Adiciona nós
+    for node in data['nodes']:
+        node_id = node['data']['id']
+        G.add_node(node_id)
+    # Adiciona arestas
+    for edge in data['edges']:
+        source = edge['data']['source']
+        target = edge['data']['target']
+        weight = edge['data'].get('weight', 1)
+        # Se o peso veio como string, converte para número
+        try:
+            weight = float(weight)
+        except Exception:
+            weight = 1
+        G.add_edge(source, target, weight=weight)
+    return G
+
+def get_networkx_graph():
+    data = get_graph_from_js()
+    G = cytoscape_to_networkx(data)
+    return G
+
 @when("click", "#add-edge")
 def add_edge():
-    get_graph_from_js()
+    G = get_networkx_graph()
+    print("Nós do NetworkX:", list(G.nodes))
+    print("Arestas do NetworkX:", list(G.edges(data=True)))
 
 @when("click", "#reset-graph")
 def reset_graph():
@@ -27,7 +52,27 @@ def export_graph(G):
 
 @when("click", "#export-graph-original")
 def export_original_graph(event):
-    pass
+    global G
+    G = get_networkx_graph()
+    if G.number_of_nodes() == 0:
+        return
+
+    # Converte o grafo para JSON
+    data = json_graph.node_link_data(G, edges="links")
+    json_data = json.dumps(data, indent=4)
+
+    # Cria um link de download no navegador
+    blob = Blob.new([json_data], {"type": "application/json"})
+    url = URL.createObjectURL(blob)
+
+    # Configura e sdispara o download
+    link = document.createElement("a")
+    link.href = url
+    link.download = "graph.json"
+    link.click()
+    URL.revokeObjectURL(url)
+
+    
 
 @when("click", "#import-graph")
 def open_file_selector(evt):
